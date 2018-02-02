898d710351d98ea5f4faade3064b3b1212c04a94
mmCAtE@skyfire <178076837@163.com>
2018-02-02 16:05:01 +0800

    添加了一个统一的入口脚步(但是内容未完成)

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# author:mmCAtE
+# url:www.wangxiaomao.net
+_type = {"1. Install Desktop Environment", "2. Install Server Environment"}
+echo "数组元素个数为: ${#_type[*]}"
+
+What will you do?\n
+
+
+read -n 1 _type
